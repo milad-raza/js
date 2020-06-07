de98bd99d03ebb163d05a0d7f97624185e0eec40
Milad Raza <miladrazabhatti@gmail.com>
2020-06-07 20:02:24 +0500

    Add files via upload

---
+++ Chapter6-9/app.js
@@ -0,0 +1,178 @@
+var num = prompt("Enter a number");
+document.write("Result:<br>");
+document.write("The value of a is: "+num+"<br>");
+document.write("------------------------------------------<br><br>");
+document.write("The value of ++a is: "+(++num)+"<br>");
+document.write("Now the value of a is: "+num +"<br><br>");
+document.write("The value of a++ is: "+(num++)+"<br>");
+document.write("Now the value of a is: "+num +"<br><br>");
+document.write("The value of --a is: "+(--num)+"<br>");
+document.write("Now the value of a is: "+num +"<br><br>");
+document.write("The value of a-- is: "+(num--)+"<br>");
+document.write("Now the value of a is: "+num +"<br><br><br>");
+
+var a=2, b=1;
+document.write("a is: "+a+"<br>");
+document.write("b is: "+b+"<br>");
+document.write("--a is: "+(--a)+"<br>");
+var a=2 ,b=1;
+document.write("--a - --b is: "+(--a - --b)+"<br>");
+var a=2 ,b=1;
+document.write("--a - --b + ++b is: "+(--a - --b + ++b)+"<br>");
+var a=2,b=1;
+document.write("--a - --b + ++b + b-- is: "+(--a - --b + ++b + b--)+"<br>");
+var a=2,b=1;
+var result =(--a - --b + ++b + b--);
+document.write("Result is: "+result+"<br><br><br>");
+
+var well = prompt("Enter Your Name");
+alert("Welcome "+well);
+
+var table = prompt("Enter a number (1-10)");
+if (table == 1){
+document.write("1 * 1 = "  + 1*1 + "<br>");
+document.write("1 * 2 = "  + 1*2 + "<br>");
+document.write("1 * 3 = "  + 1*3 + "<br>");
+document.write("1 * 4 = "  + 1*4 + "<br>");
+document.write("1 * 5 = "  + 1*5 + "<br>");
+document.write("1 * 6 = "  + 1*6 + "<br>");
+document.write("1 * 7 = "  + 1*7 + "<br>");
+document.write("1 * 8 = "  + 1*8 + "<br>");
+document.write("1 * 9 = "  + 1*9 + "<br>");
+document.write("1 * 10 = "  + 1*10 + "<br> <br>");
+}
+else if (table==2){
+document.write("2 * 1 = "  + 2*1 + "<br>");
+document.write("2 * 2 = "  + 2*2 + "<br>");
+document.write("2 * 3 = "  + 2*3 + "<br>");
+document.write("2 * 4 = "  + 2*4 + "<br>");
+document.write("2 * 5 = "  + 2*5 + "<br>");
+document.write("2 * 6 = "  + 2*6 + "<br>");
+document.write("2 * 7 = "  + 2*7 + "<br>");
+document.write("2 * 8 = "  + 2*8 + "<br>");
+document.write("2 * 9 = "  + 2*9 + "<br>");
+document.write("2 * 10 = "  + 2*10 + "<br>");
+}
+else if (table==3){
+document.write("3 * 1 = "  + 3*1 + "<br>");
+document.write("3 * 2 = "  + 3*2 + "<br>");
+document.write("3 * 3 = "  + 3*3 + "<br>");
+document.write("3 * 4 = "  + 3*4 + "<br>");
+document.write("3 * 5 = "  + 3*5 + "<br>");
+document.write("3 * 6 = "  + 3*6 + "<br>");
+document.write("3 * 7 = "  + 3*7 + "<br>");
+document.write("3 * 8 = "  + 3*8 + "<br>");
+document.write("3 * 9 = "  + 3*9 + "<br>");
+document.write("3 * 10 = "  + 3*10 + "<br>");
+}
+else if (table==4){
+document.write("4 * 1 = "  + 4*1 + "<br>");
+document.write("4 * 2 = "  + 4*2 + "<br>");
+document.write("4 * 3 = "  + 4*3 + "<br>");
+document.write("4 * 4 = "  + 4*4 + "<br>");
+document.write("4 * 5 = "  + 4*5 + "<br>");
+document.write("4 * 6 = "  + 4*6 + "<br>");
+document.write("4 * 7 = "  + 4*7 + "<br>");
+document.write("4 * 8 = "  + 4*8 + "<br>");
+document.write("4 * 9 = "  + 4*9 + "<br>");
+document.write("4 * 10 = "  + 4*10 + "<br>");
+}
+else if (table==5){
+document.write("5 * 1 = "  + 5*1 + "<br>");
+document.write("5 * 2 = "  + 5*2 + "<br>");
+document.write("5 * 3 = "  + 5*3 + "<br>");
+document.write("5 * 4 = "  + 5*4 + "<br>");
+document.write("5 * 5 = "  + 5*5 + "<br>");
+document.write("5 * 6 = "  + 5*6 + "<br>");
+document.write("5 * 7 = "  + 5*7 + "<br>");
+document.write("5 * 8 = "  + 5*8 + "<br>");
+document.write("5 * 9 = "  + 5*9 + "<br>");
+document.write("5 * 10 = "  + 5*10 + "<br>");
+}
+else if (table==6){
+document.write("6 * 1 = "  + 6*1 + "<br>");
+document.write("6 * 2 = "  + 6*2 + "<br>");
+document.write("6 * 3 = "  + 6*3 + "<br>");
+document.write("6 * 4 = "  + 6*4 + "<br>");
+document.write("6 * 5 = "  + 6*5 + "<br>");
+document.write("6 * 6 = "  + 6*6 + "<br>");
+document.write("6 * 7 = "  + 6*7 + "<br>");
+document.write("6 * 8 = "  + 6*8 + "<br>");
+document.write("6 * 9 = "  + 6*9 + "<br>");
+document.write("6 * 10 = "  + 6*10 + "<br>");
+}
+else if (table==7){
+document.write("7 * 1 = "  + 7*1 + "<br>");
+document.write("7 * 2 = "  + 7*2 + "<br>");
+document.write("7 * 3 = "  + 7*3 + "<br>");
+document.write("7 * 4 = "  + 7*4 + "<br>");
+document.write("7 * 5 = "  + 7*5 + "<br>");
+document.write("7 * 6 = "  + 7*6 + "<br>");
+document.write("7 * 7 = "  + 7*7 + "<br>");
+document.write("7 * 8 = "  + 7*8 + "<br>");
+document.write("7 * 9 = "  + 7*9 + "<br>");
+document.write("7 * 10 = "  + 7*10 + "<br>");
+}
+else if (table==8){
+document.write("8 * 1 = "  + 8*1 + "<br>");
+document.write("8 * 2 = "  + 8*2 + "<br>");
+document.write("8 * 3 = "  + 8*3 + "<br>");
+document.write("8 * 4 = "  + 8*4 + "<br>");
+document.write("8 * 5 = "  + 8*5 + "<br>");
+document.write("8 * 6 = "  + 8*6 + "<br>");
+document.write("8 * 7 = "  + 8*7 + "<br>");
+document.write("8 * 8 = "  + 8*8 + "<br>");
+document.write("8 * 9 = "  + 8*9 + "<br>");
+document.write("8 * 10 = "  + 8*10 + "<br>");
+}
+else if (table==9){
+document.write("9 * 1 = "  + 9*1 + "<br>");
+document.write("9 * 2 = "  + 9*2 + "<br>");
+document.write("9 * 3 = "  + 9*3 + "<br>");
+document.write("9 * 4 = "  + 9*4 + "<br>");
+document.write("9 * 5 = "  + 9*5 + "<br>");
+document.write("9 * 6 = "  + 9*6 + "<br>");
+document.write("9 * 7 = "  + 9*7 + "<br>");
+document.write("9 * 8 = "  + 9*8 + "<br>");
+document.write("9 * 9 = "  + 9*9 + "<br>");
+document.write("9 * 10 = "  + 9*10 + "<br>");
+}
+else if (table==10){
+document.write("10 * 1 = "  + 10*1 + "<br>");
+document.write("10 * 2 = "  + 10*2 + "<br>");
+document.write("10 * 3 = "  + 10*3 + "<br>");
+document.write("10 * 4 = "  + 10*4 + "<br>");
+document.write("10 * 5 = "  + 10*5 + "<br>");
+document.write("10 * 6 = "  + 10*6 + "<br>");
+document.write("10 * 7 = "  + 10*7 + "<br>");
+document.write("10 * 8 = "  + 10*8 + "<br>");
+document.write("10 * 9 = "  + 10*9 + "<br>");
+document.write("10 * 10 = "  + 10*10 + "<br>");
+}
+else{
+document.write("5 * 1 = "  + 5*1 + "<br>");
+document.write("5 * 2 = "  + 5*2 + "<br>");
+document.write("5 * 3 = "  + 5*3 + "<br>");
+document.write("5 * 4 = "  + 5*4 + "<br>");
+document.write("5 * 5 = "  + 5*5 + "<br>");
+document.write("5 * 6 = "  + 5*6 + "<br>");
+document.write("5 * 7 = "  + 5*7 + "<br>");
+document.write("5 * 8 = "  + 5*8 + "<br>");
+document.write("5 * 9 = "  + 5*9 + "<br>");
+document.write("5 * 10 = "  + 5*10 + "<br> <br> <br>");
+}
+var full = 100;
+var s1 = prompt("Enter first subject name");
+var s2 = prompt("Enter second subject name");
+var s3 = prompt("Enter third subject name");
+var m1 = prompt("Enter first subject obtained marks");
+var m2 = prompt("Enter second subject obtained marks");
+var m3 = prompt("Enter third subject obtained marks");
+var per1 = (m1/full)*100;
+var per2 = (m2/full)*100;
+var per3 = (m3/full)*100;
+var fullall = 100*3;
+var mall = (+m1)+(+m2)+(+m3);
+var perall = (mall/fullall)*100;
+document.write("<table> <th>Subjects</th> <th>Total marks</th> <th>Obtained marks</th> <th>Percentage</th> <tr> <td>"+s1+"</td>" + "<td>"+full+"</td>"+ "<td>"+m1+"</td>"+"<td>"+per1+"%"+"</td>"+"</tr>"+"<tr><td>"+s2+"</td>" + "<td>"+full+"</td>"+ "<td>"+m2+"</td>"+"<td>"+per2+"%"+"</tr>"+"<tr><td>"+s3+"</td>" + "<td>"+full+"</td>"+ "<td>"+m1+"</td>"+"<td>"+per1+"%"+"</tr>"+"<tr>"+"<td></td>"+"<td><b>"+fullall+"</b></td>"+"<td><b>"+mall+"</b></td>"+"<td><b>"+perall+"%"+"</b><td>"+"</tr>"+"</table>");
+
