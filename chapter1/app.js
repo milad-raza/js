dd5a0b4790117ced99ada9d31e5905fc305111cc
Milad Raza <miladrazabhatti@gmail.com>
2020-06-13 22:51:39 +0500

    .

--- chapter1/app.js
+++
@@ -1,6 +0,0 @@
-alert("welcome");
-alert("Error! please enter a valid password");
-alert("Welcome to js Land...\nHappy Coding!");
-alert("Welcome to js Land...");
-alert("Happy Coding!\n□ prevent this page for creating additional dialogues");
-console.log("Hello... I can run JS through my web browser's console");
