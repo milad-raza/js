abb21d80d9ce37a868d2b339ce3fb7fb119e5ef7
Milad Raza <miladrazabhatti@gmail.com>
2020-06-13 22:53:35 +0500

    .

--- chapter4/app.js
+++
@@ -1,8 +0,0 @@
-var x=1 , y=2 , z=3;
-//var a1=1 ,usevar=2 , x3=y , d4="s" , z56="g";
-//var 1=a , 2b=b , if=c, d f=d, d+&=e;
-document.write(" <h1> Rules for naming JS variables </h1> <br><br><br>");
-document.write("Variable names can only contain letters,numbers,$ and _. <br> For example: $_my1stvariable <br>");
-document.write("Variable must begin with a letter,$ or _. <br> For example: $name,_name or name <br>");
-document.write("Variable names are case sensitive <br>");
-document.write("Variable names should not be JS keywords");
